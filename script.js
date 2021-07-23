439581ccb0e30c0a049c7d064815a0989734e1cc
Jorjeee3 <look7at7george@gmail.com>
2021-07-23 21:53:14 +0300

    finish spotlight slider

--- script.js
+++ script.js
@@ -2,42 +2,69 @@
 $(document).ready(function () {
    $('.header-burger').click(function (event) {
       $('.header-nav').removeClass('hidden');
-      $("body").css("overflow", "hidden");
+      $('body').css('overflow', 'hidden');
+      $('header-nav').css('z-index', '10');
+      $('slider-image').css('display', 'none');
    });
 
    $('.close-cross').click(function (event) {
       $('.header-nav').addClass('hidden');
-      $("body").css("overflow", "auto");
+      $('body').css('overflow', 'auto');
    });
 
 
 
 
-   $('.slider').slick({
-      dots: false,
+   // $('.slider').slick({
+   //    dots: false,
+   //    infinite: true,
+   //    speed: 300,
+   //    slidesToShow: 1,
+   //    slidesToScroll: 1,
+   //    arrows: false,
+   //    variableWidth: true,
+   //    responsive: [
+   //       {
+   //          breakpoint: 1024,
+   //          settings: {
+   //             slidesToShow: 1,
+   //             slidesToScroll: 1,
+   //             infinite: true,
+   //          }
+   //       },
+   //       {
+   //          breakpoint: 900,
+   //          settings: "unslick",
+   //          prevArrow: false,
+   //          nextArrow: false,
+   //          draggable: false
+   //       }
+   //    ]
+   // });
+
+   $('.spotlight-list').slick({
       infinite: true,
-      speed: 300,
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
-      variableWidth: true,
       responsive: [
          {
             breakpoint: 1024,
             settings: {
-               slidesToShow: 1,
-               slidesToScroll: 1,
-               infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
             }
          },
          {
             breakpoint: 900,
             settings: "unslick",
-            prevArrow: false,
-            nextArrow: false,
-            draggable: false
          }
       ]
    });
+
 })
+
+
+
 
